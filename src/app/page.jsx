5699fc1cd6d24c70d41bbb5b57a3f55845a49e05
Tanishq Solanki <officialxinov@gmail.com>
2024-03-01 17:29:07 +0530

    initial commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -66,7 +66,7 @@ export default function Home() {
               </div>
 
 
-              <div className="relative flex justify-start items-start sm:items-start md:items-center lg:items-center xl:items-center w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2 h-full px-0 sm:px-0 md:px-8 lg:px-8 xl:px-8 pt-12 sm:pt-12 md:pt-2 lg:pt-2 xl:pt-2 py-2 overflow-hidden">
+              <div className="relative flex justify-start items-start sm:items-start md:items-center lg:items-center xl:items-center w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2 h-full px-0 sm:px-0 md:px-8 lg:px-8 xl:px-8 pt-8 sm:pt-8 md:pt-2 lg:pt-2 xl:pt-2 py-2 overflow-hidden">
                 <div className="block justify-start items-center w-full h-auto">
                   <div className="hidden sm:hidden md:block lg:block xl:block justify-start items-center w-auto text-[2.8rem] font-bold text-[#0f102e] leading-none tracking-[0.01em] text-start">
                     <span className="flex justify-start items-center">
@@ -77,11 +77,23 @@ export default function Home() {
                     </span>
                   </div>
 
-                  <div className="block sm:block md:hidden lg:hidden xl:hidden justify-start items-center w-full text-[1.85rem] font-bold text-[#0f102e] leading-none tracking-[0.01em] text-center">
+                  <div className="block sm:block md:hidden lg:hidden xl:hidden justify-start items-center w-full text-[1.65rem] font-bold text-[#0f102e] leading-none tracking-[0.01em] text-center">
                     Learn the&nbsp;<span className="text-[#1d8d37] font__spline">deep-rooted</span>&nbsp;knowledege from the&nbsp;<span className="text-[#1d8d37] font__spline">experts.</span>
                   </div>
 
-                  <div className="flex justify-center sm:justify-center md:justify-start lg:justify-start xl:justify-start items-center w-full h-12 mt-4 text-lg sm:text-lg md:text-base lg:text-base xl:text-base">
+                  <div className="absolute top-[8.5rem] flex justify-center items-center w-full">
+                    <div className="absolute z-[1] flex sm:flex md:hidden lg:hidden xl:hidden justify-center items-center w-full h-0.5" id="index_page__gradient02" />
+
+                    <div className="absolute z-[2] flex sm:flex md:hidden lg:hidden xl:hidden justify-center items-center w-full h-auto">
+                      <div className="flex justify-center items-center w-6 h-6 bg-white border-[1.5px] border-[#1d8d37] ring-1 ring-[#1d8d3780] ring-offset-1 rounded-full text-[#1d8d37]">
+                        <svg className="flex justify-end items-center w-4 h-4" fill="currentColor" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex justify-center sm:justify-center md:justify-start lg:justify-start xl:justify-start items-center w-full h-12 mt-12 text-lg sm:text-lg md:text-base lg:text-base xl:text-base">
                     <Link href="/courses" className="flex justify-center items-center w-auto h-full">
                       <button className="flex justify-center items-center w-auto h-full px-6 bg-[#0f102e] rounded-md border border-[#0f102e] hover:bg-[#1a1c48] active:border-[#0c0d26] focus:border-[#0c0d26] focus:ring-[2.5px] focus:ring-[#9cb9d9] text-white font-medium duration-100  no-focus select-none">
                         Explore Courses
